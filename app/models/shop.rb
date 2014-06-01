05e7b8892a50881d04ef6dc6167ad3b93c0fa4cb
ilynne <ilynne@gmail.com>
2014-05-31 17:23:05 -0700

    validate presence of name on shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,4 @@
 class Shop < ActiveRecord::Base
   has_many :products
+  validates :name, presence: true
 end
--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Shop do
   it { should have_many :products }
+  it { should validate_presence_of(:name) }
 end
